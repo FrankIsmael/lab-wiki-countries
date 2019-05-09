037ab48abf7075f8a613a4afd7687fd385c75943
Ismael <ismaelfcom93@gmail.com>
2019-05-09 12:14:12 -0500

    hice poquito

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import countries from './countries.json'
 
 class App extends Component {
   render() {
+    console.log(countries)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <div className="row">
+          <div class="col-5" style={{ height:'80vh', overflow: 'scroll' }}>
+            <div className="list-group">
+              {countries.map((eachCountry, index) => {
+                return (
+                  <div className="list-group-item list-group-item-action" key={eachCountry.index} >
+                    <h3>
+                      {eachCountry.name.common}
+                    </h3>
+                    <h4>{eachCountry.cca3}</h4>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+        {/* <Switch>
+        <Route exact path='/' component={CountryDetail} />
+      </Switch> */}
       </div>
     );
   }
